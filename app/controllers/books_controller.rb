81c3959f2deb0215d449c02df7a91854cf9dd29c
hikarook94 <walrus4l145@icloud.com>
2023-04-12 19:22:55 +0900

    status: :unprocessable_entityをつけた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,9 @@ class BooksController < ApplicationController
       redirect_to books_path
     else
       flash.now[:alert] = '登録に失敗しました。'
-      render :new
+      render :new, status: :unprocessable_entity 
+      # status: :unprocessable_entity をつけないとバリデーション失敗時のエラーメッセージが表示されない。
+      # https://qiita.com/jnchito/items/5c41a7031404c313da1f#%E3%83%90%E3%83%AA%E3%83%87%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E5%A4%B1%E6%95%97%E6%99%82%E3%81%AB-status-unprocessable_entity-%E3%82%92%E4%BB%98%E3%81%91%E3%82%8B%E5%BF%85%E8%A6%81%E3%81%8C%E3%81%82%E3%82%8B
     end
   end
 
